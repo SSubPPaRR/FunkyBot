b7c5228d2ca5e393ef331796e30abd6ee270c356
clayton <clayton.olaria@gmail.com>
2022-11-27 21:52:32 -0400

    new py

--- main.py
+++ main.py
@@ -90,8 +90,7 @@ async def pause(ctx):
     await ctx.send(embed=embed)
 
 
-@client.command(name="resume")
-async def resume(ctx):
+@client.command(name="resume")async def resume(ctx):
     player = music.get_player(guild_id=ctx.guild.id)
     song = await player.resume()
     await ctx.send(embed=standard_embed(ctx, f"▶ Resumed {song.name}"))
